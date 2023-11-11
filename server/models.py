a9cfc84125b0a47af1d3ff99c2231a3e3e189bfa
Mheyrie <omota4fidelity@gmail.com>
2023-11-11 12:31:08 +0100

    not done

--- server/models.py
+++ server/models.py
@@ -12,6 +12,23 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if name=='':
+            raise ValueError('Input name')
+        if Author.query.filter(Author.name==name).first()==False:
+            raise ValueError('Name already exists')
+        return name
+       
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) < 10 or len(phone_number) >10 or isinstance(phone_number, int) == False:
+            raise ValueError("requires each phone number to be exactly ten digits.")
+        return phone_number
+
+
+
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +45,25 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('title')
+    def validate_name(self, key, title):
+        if not title:
+            raise ValueError("Input title")
+        return title
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250 or len(content) > 250 :
+            raise ValueError("Content too short test. Less than 250 chars.")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Post summary is a maximum of 250 characters")
+        return summary
+
+
 
 
     def __repr__(self):
